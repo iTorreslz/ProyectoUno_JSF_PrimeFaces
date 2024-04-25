60f5d3f1a12d722ba91690c65ee46d639ea3e776
iTorres <>
2024-04-25 14:19:53 +0200

    commit_014

---
+++ src/java/com/inerttia/Items.java
@@ -0,0 +1,409 @@
+package com.inerttia;
+
+import es.inerttia.ittws.controllers.ArticuloClasificacionController;
+import es.inerttia.ittws.controllers.ArticuloController;
+import es.inerttia.ittws.controllers.ClasificacionController;
+import es.inerttia.ittws.controllers.PaisController;
+import es.inerttia.ittws.controllers.TipoArticuloController;
+import es.inerttia.ittws.controllers.TipoPickingController;
+import es.inerttia.ittws.controllers.entities.ArticuloClasificacion;
+import es.inerttia.ittws.controllers.entities.Clasificacion;
+import es.inerttia.ittws.controllers.entities.Pais;
+import es.inerttia.ittws.controllers.entities.TipoArticulo;
+import es.inerttia.ittws.controllers.entities.TipoPicking;
+import es.inerttia.ittws.controllers.entities.custom.Articulo;
+import es.inerttia.ittwscomun.Configuracion;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Iván Torres
+ */
+@ManagedBean(name = "Items")
+@ViewScoped
+public class Items {
+
+    //------------------------------------------------------------------------//
+    //                               VARIABLES                                //
+    //------------------------------------------------------------------------//
+    // LAZY TABLE
+    private LazyProductosDataModel lazyItems;
+    
+    // ARTICULOS
+    private List<Articulo> articulos;
+
+    private int conTerceroDep;
+    private int idArticulo;
+    private String descripcion;
+    private String referencia;
+    private int codBarras;
+    private String clasificacion;
+    private List<Clasificacion> clasificaciones;
+    private int estado;
+
+    private int familia;
+    private int marca;
+    private int nivelesClasificacion;
+    private int bloqueo;
+    private int grupoArticulos;
+    private int etiquetaDe;
+    private List<Pais> paises;
+    private int etiquetado;
+    private int controlLote;
+    private int controlCaducidad;
+    private int pendienteVerificacion;
+    private int fechaUltModificacion;
+    private int pesoBruto;
+    private int volumenBruto;
+    private int controlSeries;
+    private int tipoArticulo;
+    private List<TipoArticulo> tiposArticulo;
+    private int stock;
+    private int preventa;
+    private int tipoPicking;
+    private List<TipoPicking> tiposPicking;
+    private int artClasificacion;
+    private List<ArticuloClasificacion> articulosClasif;
+    private double maxPesoBruto;
+    private double minPesoBruto;
+
+    //------------------------------------------------------------------------//
+    //                                  INIT                                  //
+    //------------------------------------------------------------------------//
+    //
+    @PostConstruct
+    public void init() {
+        Configuracion conf = new Configuracion();
+        ArticuloController ctl = new ArticuloController(conf);
+//        ctl.getArticulosConsultaGeneral(0, 0, idPaisEtiquetado, idArticulo, descripcion, referencia, codigoBarras, idClasificacion,
+//                0, 0, 0, terceros, familias, marcas, niveles, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, idPaisEtiqueta, 0, 0, 0, 0);
+
+        clasificacion = "-1";
+    }
+
+    //------------------------------------------------------------------------//
+    //                           MÉTODOS DEL BEAN                             //
+    //------------------------------------------------------------------------//
+    //
+    // BUSCAR ARTÍCULOS
+    public void searchItems() {
+        
+    }
+    
+    //------------------------------------------------------------------------//
+    //                           GETTERS Y SETTERS                            //
+    //------------------------------------------------------------------------//
+    //
+    public LazyProductosDataModel getLazyItems() {
+        return lazyItems;
+    }
+
+    public void setLazyItems(LazyProductosDataModel lazyItems) {
+        this.lazyItems = lazyItems;
+    }
+
+    public List<Articulo> getArticulos() {
+        return articulos;
+    }
+
+    public void setArticulos(List<Articulo> articulos) {
+        this.articulos = articulos;
+    }
+
+    public int getConTerceroDep() {
+        return conTerceroDep;
+    }
+
+    public void setConTerceroDep(int conTerceroDep) {
+        this.conTerceroDep = conTerceroDep;
+    }
+
+    public int getIdArticulo() {
+        return idArticulo;
+    }
+
+    public void setIdArticulo(int idArticulo) {
+        this.idArticulo = idArticulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public int getCodBarras() {
+        return codBarras;
+    }
+
+    public void setCodBarras(int codBarras) {
+        this.codBarras = codBarras;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public List<Clasificacion> getClasificaciones() {
+        if (clasificaciones == null) {
+            Configuracion conf = new Configuracion();
+            ClasificacionController ctl = new ClasificacionController(conf);
+            clasificaciones = ctl.getClasificaciones();
+            conf.cerrar();
+        }
+        return clasificaciones;
+    }
+
+    public void setClasificaciones(List<Clasificacion> clasificaciones) {
+        this.clasificaciones = clasificaciones;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getFamilia() {
+        return familia;
+    }
+
+    public void setFamilia(int familia) {
+        this.familia = familia;
+    }
+
+    public int getMarca() {
+        return marca;
+    }
+
+    public void setMarca(int marca) {
+        this.marca = marca;
+    }
+
+    public int getNivelesClasificacion() {
+        return nivelesClasificacion;
+    }
+
+    public void setNivelesClasificacion(int nivelesClasificacion) {
+        this.nivelesClasificacion = nivelesClasificacion;
+    }
+
+    public int getBloqueo() {
+        return bloqueo;
+    }
+
+    public void setBloqueo(int bloqueo) {
+        this.bloqueo = bloqueo;
+    }
+
+    public int getGrupoArticulos() {
+        return grupoArticulos;
+    }
+
+    public void setGrupoArticulos(int grupoArticulos) {
+        this.grupoArticulos = grupoArticulos;
+    }
+
+    public int getEtiquetaDe() {
+        return etiquetaDe;
+    }
+
+    public void setEtiquetaDe(int etiquetaDe) {
+        this.etiquetaDe = etiquetaDe;
+    }
+
+    public List<Pais> getPaises() {
+        if (paises == null) {
+            Configuracion conf = new Configuracion();
+            PaisController ctl = new PaisController(conf);
+            paises = ctl.getPaises();
+            conf.cerrar();
+        }
+        return paises;
+    }
+
+    public void setPaises(List<Pais> paises) {
+        this.paises = paises;
+    }
+
+    public int getEtiquetado() {
+        return etiquetado;
+    }
+
+    public void setEtiquetado(int etiquetado) {
+        this.etiquetado = etiquetado;
+    }
+
+    public int getControlLote() {
+        return controlLote;
+    }
+
+    public void setControlLote(int controlLote) {
+        this.controlLote = controlLote;
+    }
+
+    public int getControlCaducidad() {
+        return controlCaducidad;
+    }
+
+    public void setControlCaducidad(int controlCaducidad) {
+        this.controlCaducidad = controlCaducidad;
+    }
+
+    public int getPendienteVerificacion() {
+        return pendienteVerificacion;
+    }
+
+    public void setPendienteVerificacion(int pendienteVerificacion) {
+        this.pendienteVerificacion = pendienteVerificacion;
+    }
+
+    public int getFechaUltModificacion() {
+        return fechaUltModificacion;
+    }
+
+    public void setFechaUltModificacion(int fechaUltModificacion) {
+        this.fechaUltModificacion = fechaUltModificacion;
+    }
+
+    public int getPesoBruto() {
+        return pesoBruto;
+    }
+
+    public void setPesoBruto(int pesoBruto) {
+        this.pesoBruto = pesoBruto;
+    }
+
+    public int getVolumenBruto() {
+        return volumenBruto;
+    }
+
+    public void setVolumenBruto(int volumenBruto) {
+        this.volumenBruto = volumenBruto;
+    }
+
+    public int getControlSeries() {
+        return controlSeries;
+    }
+
+    public void setControlSeries(int controlSeries) {
+        this.controlSeries = controlSeries;
+    }
+
+    public int getTipoArticulo() {
+        return tipoArticulo;
+    }
+
+    public void setTipoArticulo(int tipoArticulo) {
+        this.tipoArticulo = tipoArticulo;
+    }
+
+    public List<TipoArticulo> getTiposArticulo() {
+        if (tiposArticulo == null) {
+            Configuracion conf = new Configuracion();
+            TipoArticuloController ctl = new TipoArticuloController(conf);
+            tiposArticulo = ctl.getTiposArticulo();
+            conf.cerrar();
+        }
+        return tiposArticulo;
+    }
+
+    public void setTiposArticulo(List<TipoArticulo> tiposArticulo) {
+        this.tiposArticulo = tiposArticulo;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getPreventa() {
+        return preventa;
+    }
+
+    public void setPreventa(int preventa) {
+        this.preventa = preventa;
+    }
+
+    public int getTipoPicking() {
+        return tipoPicking;
+    }
+
+    public void setTipoPicking(int tipoPicking) {
+        this.tipoPicking = tipoPicking;
+    }
+
+    public List<TipoPicking> getTiposPicking() {
+        if (tiposPicking == null) {
+            Configuracion conf = new Configuracion();
+            TipoPickingController ctl = new TipoPickingController(conf);
+            tiposPicking = ctl.getTiposPicking();
+            conf.cerrar();
+        }
+        return tiposPicking;
+    }
+
+    public void setTiposPicking(List<TipoPicking> tiposPicking) {
+        this.tiposPicking = tiposPicking;
+    }
+
+    public int getArtClasificacion() {
+        return artClasificacion;
+    }
+
+    public void setArtClasificacion(int artClasificacion) {
+        this.artClasificacion = artClasificacion;
+    }
+
+    public List<ArticuloClasificacion> getArticulosClasif() {
+        if (articulosClasif == null) {
+            Configuracion conf = new Configuracion();
+            ArticuloClasificacionController ctl = new ArticuloClasificacionController(conf);
+            articulosClasif = ctl.getArticuloClasificacions();
+            conf.cerrar();
+        }
+        return articulosClasif;
+    }
+
+    public void setArticulosClasif(List<ArticuloClasificacion> articulosClasif) {
+        this.articulosClasif = articulosClasif;
+    }
+
+    public double getMaxPesoBruto() {
+        return maxPesoBruto;
+    }
+
+    public void setMaxPesoBruto(double maxPesoBruto) {
+        this.maxPesoBruto = maxPesoBruto;
+    }
+
+    public double getMinPesoBruto() {
+        return minPesoBruto;
+    }
+
+    public void setMinPesoBruto(double minPesoBruto) {
+        this.minPesoBruto = minPesoBruto;
+    }
+}
